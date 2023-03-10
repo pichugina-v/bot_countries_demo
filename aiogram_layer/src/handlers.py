92ac30eb5068af26bf9ae763c2fee01de7ffaafd
Sergey Kim <sergkim7@gmail.com>
2023-03-10 17:30:30 +0300

    upd About handler

--- aiogram_layer/src/handlers.py
+++ aiogram_layer/src/handlers.py
@@ -1,7 +1,7 @@
 from aiogram import types
 
-from .app import bot, dp
-from .keyboards import MAIN_MENU
+from .app import dp
+from .keyboards import main_menu
 from .messages import ABOUT_MESSAGE
 
 
@@ -10,7 +10,7 @@ async def start_page(message: types.Message):
     """
     This handler will be called when user sends /start or /help command
     """
-    await message.reply('Главное меню:', reply_markup=MAIN_MENU)
+    await message.reply('Главное меню:', reply_markup=main_menu)
 
 
 @dp.callback_query_handler(lambda c: c.data == 'about')
@@ -19,9 +19,8 @@ async def show_about_page(callback: types.CallbackQuery):
     This handler will be called when user choose 'О проекте' in main menu.
     Shows info about bot's features.
     """
-    await bot.send_message(
-        chat_id=callback.from_user.id,
+    await callback.message.answer(
         text=ABOUT_MESSAGE,
-        reply_markup=MAIN_MENU,
+        reply_markup=main_menu,
     )
 
--- aiogram_layer/src/keyboards.py
+++ aiogram_layer/src/keyboards.py
@@ -1,20 +1,62 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-COUNTRY_SEARCH = InlineKeyboardButton(
-    'Поиск по стране', callback_data='country')
-CITY_SEARCH = InlineKeyboardButton('Поиск по городу', callback_data='city')
-ABOUT = InlineKeyboardButton('О проекте', callback_data='about')
-TO_MAIN_MENU = InlineKeyboardButton('К началу', callback_data='to_main_menu')
+buttons = {
+    'country_search': InlineKeyboardButton(
+        'Поиск по стране', callback_data='country'
+    ),
+    'city_search': InlineKeyboardButton(
+        'Поиск по городу', callback_data='city'
+    ),
+    'about': InlineKeyboardButton(
+        'О проекте', callback_data='about'
+    ),
+    'to_main_menu': InlineKeyboardButton(
+        'К началу', callback_data='to_main_menu'
+    ),
+    'weather': InlineKeyboardButton(
+        'Погода', callback_data='weather'
+    ),
+    'country': InlineKeyboardButton(
+        'Подробнее о стране', callback_data='country_info'
+    ),
+    'currency': InlineKeyboardButton(
+        'Курс валюты', callback_data='currency_rate'
+    )
+}
 
-
-WEATHER = InlineKeyboardButton('Погода', callback_data='weather')
-COUNTRY = InlineKeyboardButton(
-    'Подробнее о стране', callback_data='country_info')
-CURRENCY = InlineKeyboardButton('Курс валюты', callback_data='currency_rate')
-
-
-MAIN_MENU = InlineKeyboardMarkup().add(COUNTRY_SEARCH).add(CITY_SEARCH).add(ABOUT)
-ALL_INFO = InlineKeyboardMarkup().add(WEATHER).add(COUNTRY).add(CURRENCY).add(TO_MAIN_MENU)
-WEATHER_DETAIL = InlineKeyboardMarkup().add(COUNTRY).add(CURRENCY).add(TO_MAIN_MENU)
-COUNTRY_DETAIL = InlineKeyboardMarkup().add(WEATHER).add(CURRENCY).add(TO_MAIN_MENU)
-CURRENCY_RATE_DETAIL = InlineKeyboardMarkup().add(WEATHER).add(COUNTRY).add(TO_MAIN_MENU)
+main_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [buttons['country_search']],
+        [buttons['city_search']],
+        [buttons['about']]
+    ]
+)
+all_info = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [buttons['weather']],
+        [buttons['country']],
+        [buttons['currency']],
+        [buttons['to_main_menu']]
+    ]
+)
+weather_detail = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [buttons['country']],
+        [buttons['currency']],
+        [buttons['to_main_menu']]
+    ]
+)
+country_detail = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [buttons['weather']],
+        [buttons['currency']],
+        [buttons['to_main_menu']]
+    ]
+)
+currency_detail = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [buttons['weather']],
+        [buttons['country']],
+        [buttons['to_main_menu']]
+    ]
+)
