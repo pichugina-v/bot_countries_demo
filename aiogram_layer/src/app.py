8aa1a0572408bc8cd0e11c40b7b4ea61e5d3c5e3
Sergey Kim <sergkim7@gmail.com>
2023-03-10 15:45:39 +0300

    restructured app.py and main.py

--- aiogram_layer/src/app.py
+++ aiogram_layer/src/app.py
@@ -1,12 +1,6 @@
-import logging
-
-from aiogram import Bot, Dispatcher, executor
-from settings import TG_API_TOKEN
-
-logging.basicConfig(level=logging.INFO)
+from aiogram import Bot, Dispatcher
+from src.settings import TG_API_TOKEN
 
 
-if __name__ == '__main__':
-    bot = Bot(token=TG_API_TOKEN)
-    dp = Dispatcher(bot)
-    executor.start_polling(dp, skip_updates=True)
+bot = Bot(token=TG_API_TOKEN)
+dp = Dispatcher(bot)
