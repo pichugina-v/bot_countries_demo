d35fc1bd6aafea6a037d8f8d739c5c4d3110ced4
Alexander <dr.san77726@yandex.ru>
2023-03-10 17:22:00 +0300

    added Currency model

--- django_layer/countries_app/models.py
+++ django_layer/countries_app/models.py
@@ -53,3 +53,18 @@ class Language(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Currency(models.Model):
+    iso_code = models.CharField(max_length=30, primary_key=True, verbose_name=_('ISO code'))
+    name = models.CharField(max_length=30, verbose_name=_('name'), unique=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('updated at'))
+    country = models.ManyToManyField('Country', related_name='currencies')
+
+    class Meta:
+        verbose_name = _('currency')
+        verbose_name_plural = _('currencies')
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
