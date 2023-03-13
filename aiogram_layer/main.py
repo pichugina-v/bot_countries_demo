43e5e26d5640d2cd2fe242d15ead19f9691ea78d
georg220022 <Bhkkcfgb@yandex.ru>
2023-03-13 17:07:33 +0300

    fixed import

--- aiogram_layer/main.py
+++ aiogram_layer/main.py
@@ -4,11 +4,10 @@ import sys
 
 from aiogram import executor
 
-from aiogram_layer.src.app import dp
-
-sys.path.append(os.path.join(os.getcwd()))
 logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == '__main__':
+    sys.path.append(os.path.join(os.getcwd()))
+    from aiogram_layer.src.app import dp
     executor.start_polling(dp, skip_updates=True)
