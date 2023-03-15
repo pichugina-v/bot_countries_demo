1ae6950bcf08e3328f8da78951a18bcff2884741
Sergey Kim <sergkim7@gmail.com>
2023-03-15 17:14:26 +0300

    add admin conf

--- django_layer/countries_app/admin.py
+++ django_layer/countries_app/admin.py
@@ -1,3 +1,57 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from django_layer.countries_app.models import Capital, City, Country, Currency, Language
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'country', 'longitude', 'latitude', 'updated_at')
+    search_fields = ['name', 'country__name']
+    list_filter = (
+        ('country', admin.RelatedOnlyFieldListFilter),
+        ('updated_at', admin.DateFieldListFilter),
+    )
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('iso_code', 'name', 'population', 'area_size', 'updated_at')
+    search_fields = ['iso_code', 'name']
+    list_filter = (('updated_at', admin.DateFieldListFilter),)
+
+
+@admin.register(Capital)
+class CapitalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'country', 'city', 'updated_at')
+    search_fields = ['country__name', 'city__name']
+    list_filter = (
+        ('country', admin.RelatedOnlyFieldListFilter),
+        ('city', admin.RelatedOnlyFieldListFilter),
+        ('updated_at', admin.DateFieldListFilter),
+    )
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'countries', 'updated_at')
+    search_fields = ['name', 'country__name']
+    list_filter = (
+        ('country', admin.RelatedOnlyFieldListFilter),
+        ('updated_at', admin.DateFieldListFilter),
+    )
+
+    def countries(self, obj):
+        return [c.name for c in obj.country.all()]
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('iso_code', 'name', 'countries', 'updated_at')
+    search_fields = ['iso_code', 'name', 'country__name']
+    list_filter = (
+        ('country', admin.RelatedOnlyFieldListFilter),
+        ('updated_at', admin.DateFieldListFilter),
+    )
+
+    def countries(self, obj):
+        return [c.name for c in obj.country.all()]
