ffe58d18c8e2e82b62db4650b276ca51ba35c4a2
georg220022 <Bhkkcfgb@yandex.ru>
2023-03-18 13:00:42 +0300

    fix typing

--- cache/test/test_cache.py
+++ cache/test/test_cache.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_asyncio
 
 from cache.cache_module import Cache
 from cache.test.contains import (
@@ -10,40 +11,64 @@ from cache.test.contains import (
 
 
 class TestCache:
+    """
+    Repository test for cache
+    """
+
     @pytest.mark.asyncio()
     @pytest.mark.order(1)
-    async def test_get_city_none(self, clear_city):
+    async def test_get_city_none(self, clear_city: pytest_asyncio.fixture) -> None:
+        """
+        Test get empty city cache
+        """
         cache_response = await Cache.get_city(CITY_COORDINATES_KEY)
         assert cache_response is None
 
     @staticmethod
     @pytest.mark.asyncio
     @pytest.mark.order(2)
-    async def test_create_city():
+    async def test_create_city() -> None:
+        """
+        Test create city cache
+        """
         await Cache.create_or_update_city(CITY_DATA)
 
     @staticmethod
     @pytest.mark.asyncio
     @pytest.mark.order(3)
-    async def test_get_city():
+    async def test_get_city() -> None:
+        """
+        Test get not empty city cache
+        """
         cache_response = await Cache.get_city(CITY_COORDINATES_KEY)
         assert cache_response == CITY_DATA
 
     @pytest.mark.asyncio()
     @pytest.mark.order(4)
-    async def test_get_country_none(self, clear_country):
+    async def test_get_country_none(
+        self, clear_country: pytest_asyncio.fixture
+    ) -> None:
+        """
+        Test get empty country cache
+        """
         cache_response = await Cache.get_country(COUNTRY_COORDINATES_KEY)
         assert cache_response is None
 
     @staticmethod
     @pytest.mark.asyncio
     @pytest.mark.order(5)
-    async def test_create_country():
+    async def test_create_country() -> None:
+        """
+        Test create country cache
+        """
         await Cache.create_or_update_country(COUNTRY_DATA)
 
     @staticmethod
     @pytest.mark.asyncio
     @pytest.mark.order(6)
-    async def test_get_country():
+    async def test_get_country() -> None:
+        """
+        Test get not empty city cache
+        """
         cache_response = await Cache.get_country(COUNTRY_COORDINATES_KEY)
         assert cache_response == COUNTRY_DATA
