7d85a264ffee1efef4ddac248540515566195ff6
georg220022 <Bhkkcfgb@yandex.ru>
2023-03-26 19:55:27 +0300

    надеюсь заработает

--- cache/test/methods.py
+++ cache/test/methods.py
@@ -6,10 +6,16 @@ from cache.cache_settings import REDIS as redis
 
 
 async def create_test_data(any_key: str, any_data: pydantic.schema) -> None:
+    """
+    The function creates data in the database on request.
+    """
     await redis.set(any_key, json.dumps(dict(any_data)))
     await redis.close()
 
 
 async def clear_redis(any_key: list[str]) -> None:
+    """
+    The function removes data from the database on request.
+    """
     await redis.delete(*any_key)
     await redis.close()
--- tasks/test/fixtures.py
+++ tasks/test/fixtures.py
@@ -1,87 +1,59 @@
-# from typing import AsyncGenerator
+from typing import AsyncGenerator
 
-# import pytest_asyncio
+import pytest_asyncio
 
-# from cache.cache_module import Cache
-# from cache.test.methods import clear_redis
-# from services.repositories.api.api_schemas import CountrySchema
-# from tasks.tasks import update_currency_cache
+from cache.cache_module import Cache
+from services.repositories.api.api_schemas import CountrySchema
+from tasks.tasks import update_currency_cache
 
-# GOOD_RECORD_KEY = '123.123'
-# ONE_BAD_VALUE_KEY = '123.124'
-# ALL_BAD_VALUE_KEY = '123.125'
-# ONLY_ONE_GOOD_KEY = '123.126'
-# ONLY_ONE_BAD_KEY = '123.127'
-# BAD_RESULT = 'this is a random string'
-
-
-# @pytest_asyncio.fixture()
-# async def schema_country():
-#     return CountrySchema(
-#         iso_code='RU',
-#         name='Россия',
-#         capital='Москва',
-#         capital_longitude=152342.323424,
-#         capital_latitude=643534.3423423,
-#         area_size=4545.1111,
-#         population=145000000,
-#         currencies=dict(keys_1=BAD_RESULT, keys_2=BAD_RESULT),
-#         languages=['русский', 'английский'],
-#     )
+GOOD_RECORD_KEY = '123.123'
+ONE_BAD_VALUE_KEY = '123.124'
+ALL_BAD_VALUE_KEY = '123.125'
+ONLY_ONE_GOOD_KEY = '123.126'
+ONLY_ONE_BAD_KEY = '123.127'
+BAD_RESULT = 'this is a random string'
 
 
-# @pytest_asyncio.fixture(autouse=True)
-# async def _create_two_good_record(schema_country: pytest_asyncio.fixture) -> None:
-#     two_good_value = schema_country.copy(update=dict(currencies=dict(BYN=BAD_RESULT, TMT=BAD_RESULT)))
-#     await Cache.create_or_update_country(GOOD_RECORD_KEY, two_good_value)
+@pytest_asyncio.fixture(scope='session', autouse=False)
+async def schema_country() -> CountrySchema:
+    """
+    Fixture returns schema for test data.
+    """
+    return CountrySchema(
+        iso_code='RU',
+        name='Россия',
+        capital='Москва',
+        capital_longitude=152342.323424,
+        capital_latitude=643534.3423423,
+        area_size=4545.1111,
+        population=145000000,
+        currencies=dict(keys_1=BAD_RESULT, keys_2=BAD_RESULT),
+        languages=['русский', 'английский'],
+    )
 
 
-# @pytest_asyncio.fixture(autouse=True)
-# async def _create_one_bad_value(schema_country: pytest_asyncio.fixture) -> None:
-#     one_bad_value = schema_country.copy(update=dict(currencies=dict(BYN=BAD_RESULT, QQQ=BAD_RESULT)))
-#     await Cache.create_or_update_country(ONE_BAD_VALUE_KEY, one_bad_value)
+@pytest_asyncio.fixture(scope='session', autouse=True)
+async def create_test_data(schema_country):
+    """
+    Fixture creates test records.
+    """
+    two_good_value = schema_country.copy(update=dict(currencies=dict(BYN=BAD_RESULT, TMT=BAD_RESULT)))
+    await Cache.create_or_update_country(GOOD_RECORD_KEY, two_good_value)
+    one_bad_value = schema_country.copy(update=dict(currencies=dict(BYN=BAD_RESULT, QQQ=BAD_RESULT)))
+    await Cache.create_or_update_country(ONE_BAD_VALUE_KEY, one_bad_value)
+    two_bad_value = schema_country.copy(update=dict(currencies=dict(NNN=BAD_RESULT, QQQ=BAD_RESULT)))
+    await Cache.create_or_update_country(ALL_BAD_VALUE_KEY, two_bad_value)
+    only_one_good_value = schema_country.copy(update=dict(currencies=dict(BYN=BAD_RESULT)))
+    await Cache.create_or_update_country(ONLY_ONE_GOOD_KEY, only_one_good_value)
+    only_one_bad_value = schema_country.copy(update=dict(currencies=dict(QQQ=BAD_RESULT)))
+    await Cache.create_or_update_country(ONLY_ONE_BAD_KEY, only_one_bad_value)
+    yield
 
 
-# @pytest_asyncio.fixture(autouse=True)
-# async def _create_two_bad_value(schema_country: pytest_asyncio.fixture) -> None:
-#     two_bad_value = schema_country.copy(update=dict(currencies=dict(NNN=BAD_RESULT, QQQ=BAD_RESULT)))
-#     await Cache.create_or_update_country(ALL_BAD_VALUE_KEY, two_bad_value)
-
-
-# @pytest_asyncio.fixture(autouse=True)
-# async def _create_only_one_good_value(schema_country: pytest_asyncio.fixture) -> None:
-#     only_one_good_value = schema_country.copy(update=dict(currencies=dict(BYN=BAD_RESULT)))
-#     await Cache.create_or_update_country(ONLY_ONE_GOOD_KEY, only_one_good_value)
-
-
-# @pytest_asyncio.fixture(autouse=True)
-# async def _create_only_one_bad_value(schema_country: pytest_asyncio.fixture) -> None:
-#     only_one_bad_value = schema_country.copy(update=dict(currencies=dict(QQQ=BAD_RESULT)))
-#     await Cache.create_or_update_country(ONLY_ONE_BAD_KEY, only_one_bad_value)
-
-# # @pytest_asyncio.fixture(autouse=False)
-# # async def _start_update() -> None:
-
-
-# @pytest_asyncio.fixture(autouse=True)
-# async def prepare_and_clear_data() -> AsyncGenerator:
-#     await update_currency_cache()
-#     yield
-#     data = [GOOD_RECORD_KEY, ONE_BAD_VALUE_KEY, ALL_BAD_VALUE_KEY,
-#             ONLY_ONE_GOOD_KEY, ONLY_ONE_BAD_KEY]
-#     for indx in range(len(data)):
-#         data[indx] = f'country_{data[indx]}'
-#     await clear_redis(data)
-
-# # @pytest_asyncio.fixture(scope='package', autouse=False)
-# # async def auto_clear_cache_after_test() -> AsyncGenerator[None, None]:
-# #    """
-# #    The fixture clears records in the database after passing all the tests.
-# #    """
-
-# #    yield
-# #    data = [GOOD_RECORD_KEY, ONE_BAD_VALUE_KEY, ALL_BAD_VALUE_KEY,
-# #            ONLY_ONE_GOOD_KEY, ONLY_ONE_BAD_KEY]
-# #    for indx in range(len(data)):
-# #        data[indx] = f'country_{data[indx]}'
-# #    await clear_redis(data)
+@pytest_asyncio.fixture(scope='session', autouse=True)
+async def update_currency_test_data() -> AsyncGenerator[None, None]:
+    """
+    The fixture starts updating the exchange rate in test records.
+    """
+    await update_currency_cache()
+    yield
